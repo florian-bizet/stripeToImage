7c5b11e2fd7313ff36e7c30c9dc6d9e0c753a79c
florian-bizet <sunburnstar84@gmail.com>
2023-03-01 02:32:16 +0100

    Added user input to put images with different properties

--- Converter.java
+++ Converter.java
@@ -1,21 +1,40 @@
 import java.io.File;
+import java.util.concurrent.ExecutionException;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.awt.Graphics;
 
 public class Converter {
     
-    public Converter() {
-        // Open Folder
-        File   inputDirectory   = new File("input");
-        File[] inputImagesPaths = inputDirectory.listFiles();
+    private boolean createdSuccessfully = false;
 
-        for (File f : inputImagesPaths) {
-            BufferedImage   currentImage    = this.openImage(f);
-            BufferedImage[] currentSections = this.splitImage(currentImage);
-            BufferedImage   result          = this.mergeImage(currentSections);
-            this.exportImage(result, f.getName());
+    private int   sectionCount;
+    private int[] sectionCountArray;
+
+    public Converter(int fixedSectionCount) {
+        this.sectionCount      = 0;
+        this.sectionCountArray = null;
+
+        this.createdSuccessfully = true;
+    }
+
+    public Converter(String[] args) {
+        int i = 0;
+        this.sectionCountArray = new int[args.length];
+
+        for (String s : args) {
+            try {
+                this.sectionCountArray[i] = Integer.parseInt(s);
+                i++;
+            } catch (Exception e) {
+                System.out.println("Invalid Arguments : ");
+                System.out.println("How to use : java Converter stripeCountForAllImages OR java Converter stripeCount1 stripeCount2 ...");
+                System.out.println("The section count must be a whole number");
+                return;
+            }
         }
+
+        this.createdSuccessfully = true;
     }
 
     private BufferedImage openImage(File source) {
@@ -28,21 +47,20 @@ public class Converter {
         
     }
 
-    private BufferedImage[] splitImage(BufferedImage source) {
-        //TODO: Get section count from user input
-        int sectionCount = source.getWidth()/88;
+    private BufferedImage[] splitImage(BufferedImage source, int sectionCount) {
+        int sectionWidth = source.getWidth()/sectionCount;
 
         BufferedImage[] sectionArray = new BufferedImage[sectionCount];
         
         for (int i = 0; i < sectionCount; i++) {
-            sectionArray[i] = source.getSubimage(i*88, 0, 88, source.getHeight());
+            sectionArray[i] = source.getSubimage(i*sectionWidth, 0, sectionWidth, source.getHeight());
         }
 
         return sectionArray;
     }
 
     private BufferedImage mergeImage(BufferedImage[] sources) {
-        BufferedImage result = new BufferedImage(88, sources[0].getHeight()*sources.length, sources[0].getType());
+        BufferedImage result = new BufferedImage(sources[0].getWidth(), sources[0].getHeight()*sources.length, sources[0].getType());
 
         Graphics g = result.getGraphics();
         for (int i = 0; i < sources.length; i++) {
@@ -60,8 +78,45 @@ public class Converter {
             e.printStackTrace();
         }
     }
+
+    public void process() {
+        if (!createdSuccessfully) {return;}
+        
+        // Open Folder
+        File   inputDirectory   = new File("input");
+        File[] inputImagesPaths = inputDirectory.listFiles();
+
+        if (this.sectionCountArray != null && this.sectionCountArray.length < inputImagesPaths.length) {System.out.println("Not enough section counts. Did you forget an image ?");}
+
+        //Processing Files
+        for (int i = 0; i < inputImagesPaths.length; i++) {
+            int currentImageSectionCount = (this.sectionCountArray == null ? this.sectionCount : this.sectionCountArray[i]);
+            
+            BufferedImage   currentImage    = this.openImage(inputImagesPaths[i]);
+            BufferedImage[] currentSections = this.splitImage(currentImage, currentImageSectionCount);
+            BufferedImage   result          = this.mergeImage(currentSections);
+            this.exportImage(result, inputImagesPaths[i].getName());
+        }
+    }
     
     public static void main(String[] args) {
-        new Converter();
+        Converter converter;
+
+        if (args.length == 0) {System.out.println("How to use : java Converter stripeCountForAllImages OR java Converter stripeCount1 stripeCount2 ..."); System.out.println("The section count must be a whole number"); return;}
+
+        if (args.length > 1) {converter = new Converter(args);}
+        else                 {
+            try {
+                int count = Integer.parseInt(args[0]);
+                converter = new Converter(count);
+            } catch (Exception e) {
+                System.out.println("Invalid Arguments : ");
+                System.out.println("How to use : java Converter stripeCountForAllImages OR java Converter stripeCount1 stripeCount2 ...");
+                System.out.println("The section count must be a whole number");
+                return;
+            }
+        }
+        
+        converter.process();
     }
 }
