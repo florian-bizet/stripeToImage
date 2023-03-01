3b948079816653edf8eef5de327be7cfa4cbbfa2
florian-bizet <sunburnstar84@gmail.com>
2023-03-01 01:33:19 +0100

    Created default files

---
+++ Converter.java
@@ -0,0 +1,30 @@
+import java.awt.Image;
+
+public class Converter {
+    public Converter() {
+        //TODO: open images from input folder
+        //TODO: split them into sections
+        //TODO: merge those sections vertically
+        //TODO: save the final image in the output folder
+    }
+
+    private Image openImage(String imageName) {
+        return null;
+    }
+
+    private Image[] splitImage(Image source) {
+        return null;
+    }
+
+    private Image mergeImage(Image[] sources) {
+        return null;
+    }
+
+    private void exportImage(Image source) {
+        
+    }
+    
+    public static void main(String[] args) {
+        new Converter();
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# (WIP) stripeToImage
+
+This program converts an image splitted in stripes into its full version.
+
+It was made to convert image data found in old games files
+
+## Input :
+
+![Image to convert](input.png)
+
+## Output :
+
+![Converted image](output.png)
